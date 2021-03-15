a89db16be4cba5d2910a9478f715ba71901a2263
jan <54104803+janb-janb@users.noreply.github.com>
2021-03-15 19:17:10 +0100

    Adding '-' in name string

--- commands/help commands/help.js
+++ commands/help commands/help.js
@@ -3,9 +3,9 @@ const {MessageEmbed} = require('discord.js')
 const Helpembed = new MessageEmbed()
 .setTitle("List of Engine Commands")
 .addFields(
-  {name: `.help (.commands)`, value: `Command that lists you all bots commands!`, inline: true},
-  {name: `.challenge-help`, value: `Command that lists you all challenge commands!`, inline: false},
-  {name: `.ping (.p)`, value: `Shows latency between you and discord.`, inline: true}
+  {name: `.help (.commands) -`, value: `Command that lists you all bots commands!`, inline: true},
+  {name: `.challenge-help -`, value: `Command that lists you all challenge commands!`, inline: false},
+  {name: `.ping (.p) -`, value: `Shows latency between you and discord.`, inline: true}
 )
 .setColor(0x228ae6)
 .setFooter("Bot is Still under Development!")
