4fbab2f8749dfb02e491d9b32f57ea4491f4fb6d
jan <54104803+janb-janb@users.noreply.github.com>
2021-03-15 19:27:52 +0100

    Aliases test.

--- commands/help commands/help.js
+++ commands/help commands/help.js
@@ -3,7 +3,7 @@ const {MessageEmbed} = require('discord.js')
 const Helpembed = new MessageEmbed()
 .setTitle("List of Engine Commands")
 .addFields(
-  {name: `.help (.commands) -`, value: `Command that lists you all bots commands!`, inline: true},
+  {name: `.help (.commands or .h) -`, value: `Command that lists you all bots commands!`, inline: true},
   {name: `.challenge-help -`, value: `Command that lists you all challenge commands!`, inline: false},
   {name: `.ping (.p) -`, value: `Shows latency between you and discord.`, inline: true}
 )
@@ -11,7 +11,7 @@ const Helpembed = new MessageEmbed()
 .setFooter("Bot is Still under Development!")
 
 module.exports = {
-  aliases: ['commands'], 
+  aliases: ['commands', 'h'], 
   callback: ({ message }) => {
 
 message.reply(Helpembed)
