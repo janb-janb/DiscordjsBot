c6f9b30ec54f249434b68b22fc19b67146ebb981
jan <54104803+janb-janb@users.noreply.github.com>
2021-03-20 15:53:40 +0100

    Deleting joke

--- commands/misc/squidsrulehere.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    callback: ({ message }) => {
-        message.channel.send('<@688248267145740300> is bad person! He cant access any feature events or giveaways!')
-
-    }
-  }
