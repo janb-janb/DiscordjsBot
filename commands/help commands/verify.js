62a9445f646b7b59736ecbc8e2adf991c51e7e1e
jan <54104803+janb-janb@users.noreply.github.com>
2021-03-26 14:56:32 +0100

    Making new commands.

---
+++ commands/help commands/verify.js
@@ -0,0 +1,8 @@
+module.exports = {
+    callback: ({ message }) => {
+        message.channel.send(`There are 2 ways to verificate yourself! Check them out:
+        1: Check <#784110209823932456> and react on message with ✅
+        2: Check out <#758685100556157028> and run aq!verify lichess/chesscom <username>.`)
+
+    }
+  }
---
+++ commands/misc/dlm-bump.js
@@ -0,0 +1,6 @@
+module.exports = {
+    callback: ({ message }) => {
+        message.channel.send('Wanna bump our server? <#801013109737259029> {dlm!bump}')
+
+    }
+  }
