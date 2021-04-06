3f62b89c78e4eb0d3ade99d7fad036bd6a7c05d9
jan <54104803+janb-janb@users.noreply.github.com>
2021-04-06 17:12:00 +0200

    Fixing possible error in kick.js

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -4,10 +4,10 @@ const WOKCommands = require('wokcommands')
 module.exports = {
       callback: ({ message }) => {
 
-        if (msg.guild.member(user).hasPermission('ADMINISTRATOR')) return msg.reply('I can not kick this user, he has higher permission than I do.')
-        if (!msg.guild.me.hasPermission('KICK_MEMBERS')) return msg.reply('I need the permission `KICK_MEMBERS` for this to work.')
+        if (message.guild.member(user).hasPermission('ADMINISTRATOR')) return message.reply('I can not kick this user, he has higher permission than I do.')
+        if (!message.guild.me.hasPermission('KICK_MEMBERS')) return message.reply('I need the permission `KICK_MEMBERS` for this to work.')
         
-        msg.guild.member(user).kick()
-        msg.say('Mentioned user has been successfully kicked!' )
+        message.guild.member(user).kick()
+        msessage.channel.send('Mentioned user has been successfully kicked!' )
     }
 }
