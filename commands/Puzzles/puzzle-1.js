b31055dc584f50138bfd37cb696ac547d70bcd0b
jan <54104803+janb-janb@users.noreply.github.com>
2021-04-07 18:47:23 +0200

    Making solution response to first puzzle

--- commands/Puzzles/puzzle-1.js
+++ commands/Puzzles/puzzle-1.js
@@ -5,10 +5,21 @@ const WOKCommands = require('wokcommands')
 module.exports = {
     callback: ({ message }) => {
 
-        const attachment = new MessageAttachment('https://cdn.discordapp.com/attachments/668459390533107732/829388290049310740/unknown.png')
+        const Puzzleattachment = new MessageAttachment('https://cdn.discordapp.com/attachments/668459390533107732/829388290049310740/unknown.png')
         
         message.channel.send(`🎯New Puzzle! Checkmate in 3!
-        <hint> for solution`, attachment)
+        <hint> for solution`, Puzzleattachment)
+
+        const Solution1Attachment = new MessageAttachment('https://cdn.discordapp.com/attachments/800754062080868382/829396781288390707/unknown.png')
+
+        if (message.content.startsWith('Bg2')) {
+            message.reply(`✅Correct!
+            Go on!`, Solution1Attachment )
+
+        }
+
+    
 
     }
+
   }
