63e67d76cf07f324e35a0aed7071d0a148d4be9b
jan <54104803+janb-janb@users.noreply.github.com>
2021-04-09 23:46:17 +0200

    Kick command commit

--- commands/Puzzles/puzzle-1.js
+++ commands/Puzzles/puzzle-1.js
@@ -1,17 +1,26 @@
+const { MessageEmbed } =  require('discord.js')
 const { MessageAttachment } = require('discord.js')
-const WOKCommands = require('wokcommands')
-
-const Puzzleattachment = new MessageAttachment('https://cdn.discordapp.com/attachments/800754062080868382/829400033337409556/unknown.png')
-const PuzzleSolution = async () => {
-    await this.channel.send(`🎯New Puzzle! Checkmate in 3!
-    <hint> for solution`, Puzzleattachment)
-    .then(async message => {
-        await this.channel.awaitMessages(message => message.author.id === this.owner && !isNaN(('Bg2')), { time: 300 })
-        .then(collected => {
-            this.channel.send(`Correct! Go on!`);
-        })
-        .catch(collected => {
-            this.channel.send(`You didn't answer puzzle on time, Good Luck next time!`);
-    })
+const embed = new MessageEmbed({
+  title: 'Puzzle #1! White to move and mate in 3!'
 })
+const NewPuzzle = new PuzzleEmbed(embed)
+const Puzzleattachment = new MessageAttachment('https://cdn.discordapp.com/attachments/800754062080868382/829400033337409556/unknown.png')
+  
+.addFields(
+  { value: 'White to move and checkmate in 3! Good Luck!'},
+)
+
+.setImage(Puzzleattachment)
+
+const init NewPuzzleFn: DiscordPromptFunction<AgeMenuData> = async (message: Message, data: AgeMenuData) => {
+  const { content } = message
+  if (content === 'Bg2') {
+    message.reply('Correct go on!')
+    
+  } else {
+    message.reply('Incorrect! Try again!')
+
+  }
+  return data
 }
+ 
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -5,7 +5,7 @@ module.exports = {
       callback: ({ message, args }) => {
 
         if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
-        let User = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+        let User = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0])
         if (!User) return message.channel.send("Invalid User")
         if (User.hasPermission("KICK_MEMBERS")) return message.reply("Invalid Permissions")
         let kickReason = args.join(" ").slice(22);
