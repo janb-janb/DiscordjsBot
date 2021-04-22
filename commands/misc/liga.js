8557a4ef8abe601ed81ba7d1e65504800351c923
jan <54104803+janb-janb@users.noreply.github.com>
2021-04-22 18:54:42 +0200

    Liga.js

---
+++ commands/misc/liga.js
@@ -0,0 +1,8 @@
+module.exports = {
+    callback: ({ message }) => {
+        message.channel.send(`Make sure to join Liga for us! Quarantane Liga 17A!
+        Team: https://lichess.org/team/chesscord-discord-community
+        Tourney: https://lichess.org/tournament/PyMcPFWA `)
+
+    }
+  }
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -6,13 +6,11 @@ module.exports = {
 
         if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("Invalid Permissions")
         let User = message.guild.member(message.mentions.users.first()) || message.guild.member(args[0])
-        if (!User) return message.channel.send("Invalid User")
+        if (!User) return message.channel.send("Please specify someone to kick!")
         if (User.hasPermission("KICK_MEMBERS")) return message.reply("Invalid Permissions")
         let kickReason = args.join(" ").slice(22);
         if (!kickReason) {
           kickReason = "None"
-        } else {
-            message.channel.send(`${tag} Please specify someone to kick.`)
         }
         
         User.kick({reason: kickReason})
