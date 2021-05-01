2bbcec7d37a085b8bdc1c6b2dee1d10d0f5b07f2
Zach Rich <sniperzach075@gmail.com>
2021-05-01 11:41:06 -0500

    Puzzle Dev

--- commands/Puzzles/puzzle-1/puzzle-1.js
+++ commands/Puzzles/puzzle-1/puzzle-1.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'Under developement',
     aliases: ['puzzle-1'],
     callback: ({ message }) => {
-      var attachment = new MessageAttachment("puzzle1-1.png");
+      var attachment = new MessageAttachment("https://imgur.com/a/DqAb7JF");
       message.channel.send('Find the best move for black. Please use .answer followed by your move.', attachment)
     }
   }
