068aaf67bb8e31dd3ecc34b5c40857f06fbba621
Zach Rich <sniperzach075@gmail.com>
2021-05-01 09:47:47 -0500

    Puzzle Development

---
+++ commands/puzzle-1/puzzle-1.js
@@ -0,0 +1,7 @@
+module.exports = {
+    category: 'Puzzles',
+    description: 'Under developement',
+    callback: ({ message }) => {
+      message.channel.send('Please wait until development has finished to use this command.')
+    }
+  }
