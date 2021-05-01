a96edc563a0e8f9142636e3cbe32414ffdfb2ade
Zach Rich <sniperzach075@gmail.com>
2021-05-01 10:41:53 -0500

    Puzzle Development

--- commands/Puzzles/puzzle-1/puzzle-commands.js
+++ commands/Puzzles/puzzle-1/puzzle-commands.js
@@ -1,4 +1,4 @@
-var puzzleNumbers = [1]; //add more numbers as more puzzles are added
+var puzzleNumbers = ['1']; //add more numbers as more puzzles are added
 const puzzleRandomizer = array[Math.floor(Math.random() * puzzleNumbers.length)];
 module.exports = {
     category: 'Puzzles',
