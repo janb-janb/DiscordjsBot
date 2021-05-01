9aea7ff85732b749aad314b931484205568c5751
Zach Rich <sniperzach075@gmail.com>
2021-05-01 12:35:18 -0500

    Puzzle Dev

--- commands/Puzzles/puzzle-1/puzzle-1.js
+++ commands/Puzzles/puzzle-1/puzzle-1.js
@@ -5,10 +5,10 @@ module.exports = {
     aliases: ['puzzle-1'],
     callback: ({ message }) => {
       message.channel.send('Find the best move for black. Please use .ans followed by your move. https://imgur.com/hjxGZ4H');
-      var answer = true;
+      answer = true;
       }
     }
-    
+
 if(message.content == 'Bxd1'){
   if(answer = true){
     message.channel.send('Great! 1. Ree1 Bxd1 2. Rxd1. Please use .ans followed by your move. https://imgur.com/nnXNCwT');
