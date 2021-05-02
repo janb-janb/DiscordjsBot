f03884d9075bf1079d31fb89a647b0844e7b2938
Zach Rich <sniperzach075@gmail.com>
2021-05-02 07:50:35 -0500

    Puzzle Dev

--- commands/Puzzles/puzzle-1/answer-1.js
+++ commands/Puzzles/puzzle-1/answer-1.js
@@ -2,7 +2,7 @@ if(answer = true){
 module.exports = {
     category: 'Puzzles',
     description: 'Under developement',
-    aliases: ['puzzle-1'],
+    aliases: ['ans-Bxd1'],
     callback: ({ message }) => {
         message.channel.send('Great! 1. Ree1 Bxd1 2. Rxd1. Please use .ans followed by your move. https://imgur.com/nnXNCwT');
       }
