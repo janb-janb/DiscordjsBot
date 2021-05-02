fba93f8f404e2e66d845340befb9240dfe4233f0
jan <54104803+janb-janb@users.noreply.github.com>
2021-05-02 19:26:27 +0200

    Changing liga link

--- commands/misc/liga.js
+++ commands/misc/liga.js
@@ -2,7 +2,7 @@ module.exports = {
     callback: ({ message }) => {
         message.channel.send(`Make sure to join Liga for us! Quarantane Liga 17A!
         Team: https://lichess.org/team/chesscord-discord-community
-        Tourney: https://lichess.org/tournament/PyMcPFWA `)
+        Tourney: https://lichess.org/tournament/KF7iqcAA `)
 
     }
   }
