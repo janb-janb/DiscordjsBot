5f1fd95e791b52db3cda0562e41790a79ebd174d
jan <54104803+janb-janb@users.noreply.github.com>
2021-06-01 15:00:19 +0200

    Deleting liga command as its not needed anymore

--- commands/misc/liga.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    callback: ({ message }) => {
-        message.channel.send(`Make sure to join Liga for us! Quarantane Liga 17A!
-        Team: https://lichess.org/team/chesscord-discord-community
-        Tourney: https://lichess.org/tournament/PyMcPFWA `)
-
-    }
-  }
