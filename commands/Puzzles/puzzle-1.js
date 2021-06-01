23ecfcdc76c993eafcc8db47ea76bacf5b136ac0
jan <54104803+janb-janb@users.noreply.github.com>
2021-06-01 15:15:11 +0200

    Fixing puzzle command

--- commands/Puzzles/puzzle-1.js
+++ commands/Puzzles/puzzle-1.js
@@ -5,11 +5,10 @@ module.exports = {
     aliases: ['puzzle-1'],
     callback: ({ message }) => {
       message.channel.send('Find the best move for black. Please use .ans followed by your move. https://imgur.com/hjxGZ4H');
-      answer = 'Something';
       }
     }
+    answer = 'Something';
 
-    if(message.content == 'Something'){
   if(answer = true){
     message.channel.send('Great! 1. Ree1 Bxd1 2. Rxd1. Please use .ans followed by your move. https://imgur.com/nnXNCwT');
   }
